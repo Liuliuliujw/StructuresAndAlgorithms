5b97cd1079b93ea72831fc2003f745624dd41f70
LLLiuJW <2635285886@qq.com>
2022-02-15 16:19:24 +0800

    创建表接口List并定义必要方法

---
+++ src/list/List.java
@@ -0,0 +1,23 @@
+package list;
+
+/**
+ * 定义一个表需要实现的基本方法
+ *
+ * @author llliujw
+ */
+public interface List extends Iterable<Integer>{
+
+    void insert(int pos, Integer value);
+
+    void append(Integer value);
+
+    Integer get(int pos);
+
+    void modify(int pos, Integer value);
+
+    void remove(int pos);
+
+    void removeLast();
+
+    int size();
+}
