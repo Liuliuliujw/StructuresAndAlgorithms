4abe87deba32e8243e67d94d79a8f404652904e6
LLLiuJW <2635285886@qq.com>
2022-02-19 20:51:25 +0800

    链式队列

--- src/queue/LinkedQueue.java
+++ src/queue/LinkedQueue.java
@@ -7,21 +7,21 @@ package queue;
  */
 public class LinkedQueue<E> implements Queue<E> {
 
-    private Node<E> head;
-    private Node<E> tail;
+    private Node<E> front;
+    private Node<E> rear;
 
     LinkedQueue() {
-        head = tail = null;
+        front = rear = null;
     }
 
     @Override
     public boolean enqueue(E e) {
         Node<E> node = new Node<>(e);
-        if (tail == null) { //若队空，使队头队尾均指向新节点的引用
-            head = tail = node;
+        if (rear == null) { //若队空，使队头队尾均指向新节点的引用
+            front = rear = node;
         } else {    //若队非空，使队尾的下一节点指向新节点 并 移动队尾引用指向
-            tail.next = node;
-            tail = node;
+            rear.next = node;
+            rear = node;
         }
         return true;
     }
@@ -29,17 +29,17 @@ public class LinkedQueue<E> implements Queue<E> {
     @Override
     public E dequeue() {
         //队空返回null
-        if (head == null) return null;
+        if (front == null) return null;
         //出队
-        E e = head.data;
-        if (head.next == null) head = tail = null; //若队空 使 队头队尾均为null
-        else head = head.next; //移动队头引用指向
+        E e = front.data;
+        if (front.next == null) front = rear = null; //若队空 使 队头队尾均为null
+        else front = front.next; //移动队头引用指向
         return e;
     }
 
     @Override
     public E peek() {
-        return head == null ? null : head.data;
+        return front == null ? null : front.data;
     }
 
     private static class Node<E> {
